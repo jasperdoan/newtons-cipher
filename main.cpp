18f5ef6619c49ade0a1617671fd0422e6bb9d6c5
wluxie <49565505+wluxie@users.noreply.github.com>
2021-10-31 03:30:13 -0700

    commit

--- main.cpp
+++ main.cpp
@@ -1,170 +1,93 @@
-/*****************************************************************************
-*  AUTHOR #1  : Jasper Doan
-*  AUTHOR #2  : Andrew Huh
-*  LAB#11     : Flo's Party Planner
-*  CLASS      : CS1A
-*  SECTION    : TTh: 1:30pm
-*  DUE DATE   : Oct 30th, 2021
-*****************************************************************************/
 #include <iostream>
 #include <iomanip>
 #include <cstring>
 using namespace std;
 
-/*****************************************************************************
- *  Flo's Party Planner
- * ___________________________________________________________________________
- *  A program to help Florence decide which snacks and beverages to serve
- *        her friends. Using selection statements to determine the proper 
- *        snack and beverage recommendations the program will make
- * ___________________________________________________________________________
- *  INPUT:
- *      name                : The user's name
- *      age                 : The user's age
- *      snackChoice         : Y/N snack choice
- *      nutChoice           : Y/N nut choice
- *      chocolatePref       : User's snack preference
- *  OUTPUT:
- *      name                : The user's name
- *      drinks              : User's beverage recommondations
- *      chocolatePref       : User's snack preference
- ****************************************************************************/
+const int textlength = 7;                   // For now keep this at 7, I don't want to complicate things, just test with 6 letters words
+char arraytext[textlength];                 // INPUT from user
+char arraychar[textlength];                 // Non-repeated characters string
+char datatable[sizeof(arraychar) + 1][3];   // Data table to sort [in-progress :( ]
 
-int main()
+// Calibrate the whole sequence 
+void start()
 {
-	/***********************************************************************
-     * CONSTANTS & VARIABLES
-     * ---------------------------------------------------------------------
-     * OUTPUT - USED FOR CLASS HEADING
-     * ---------------------------------------------------------------------
-     * PROGRAMMER   : Jasper Doan
-     * PROGRAMMER   : Andrew Huh
-     * CLASS        : CS1A
-     * SECTION      : TTh: 1:30pm
-     * LAB_NUM      : LAB #13
-     * LAB_NAME     : Flo's Party Planner
-     ***********************************************************************/
-     const char  PROGRAMMER[]  = "Jasper Doan & Andrew Huh";
-     const char  CLASS[]       = "CS1A";
-     const char  SECTION[]     = "TTh: 1:30pm";
-     const int   LAB_NUM       = 13;
-     const char  LAB_NAME[]    = "Flo's Party Planner";
-
-     const int NAME_SIZE       = 25;    // PROCESSING - Size of user's name
-     const int SNACK_SIZE      = 30;    // PROCESSING - OUTPUT's snack size
-     const int DRINK_SIZE      = 6;     // PROCESSING - OUTPUT's drinks size 
-     const int COL_SIZE        = 30;    // PROCESSING - INPUT size format
-
-     char   name[NAME_SIZE];            // IN & OUT   - User's name
-     int    age;                        // IN & PROC  - User's age
-     char   drinksPref[SNACK_SIZE];     // PROC & OUT - User's drink choice
-     char   chocolatePref[SNACK_SIZE];  // PROC & OUT - User's chocolate 
-                                        //              preference
-     char   snackChoice;                // IN & PROC  - User's input on snack
-     char   nutChoice;                  // IN & PROC  - User's input on nuts
-
-	/*************************************************************************
-	*  OUTPUT - Class Heading
-     *************************************************************************/
-	cout << left;
-	cout <<"**************************************************\n";
-	cout <<"*  PROGRAMMED BY : "    << PROGRAMMER << endl;
-	cout <<"*  "      <<  setw(14)  <<  "CLASS"   << ": " << CLASS    << endl;
-	cout <<"*  "      <<  setw(14)  <<  "SECTION" << ": " << SECTION  << endl;
-	cout <<"*  AS  #" <<  setw(9)   <<  LAB_NUM   << ": " << LAB_NAME << endl;
-	cout <<"**************************************************\n\n";
-	cout << right;
-
-	/*************************************************************************
-	*  INPUT - Obtaining the user's information
-	*************************************************************************/
-     cout << left;
-     
-     cout << setw(COL_SIZE) << "What is your friends name?";
-     cin.getline(name, NAME_SIZE);
-
-     cout << setw(COL_SIZE) << "How old is your friend?";
-     cin  >> age;
-     cin.ignore(10000,'\n');
-
-     cout << setw(COL_SIZE) << "Do they like chocolate (Y/N)?";
-     cin.get(snackChoice);
-     snackChoice = toupper(snackChoice);
-     cin.ignore(10000,'\n');
-
-// cerr << "BEGIN TEST"               << endl
-//      << "Name: "  << name          << endl
-//      << "Age: "   << age           << endl
-//      << "Snack: " << snackChoice   << endl
-//      << "END TEST"                 << endl;
-
-	/*************************************************************************
-	*  PROCESSING - Deciding which snacks and beverages to serve
-     *************************************************************************/
-     
-     if(snackChoice == 'Y')
-     {
-          cout << setw(COL_SIZE) << "Do they like nuts (Y/N)?";
-          cin.get(nutChoice);
-          nutChoice = toupper(nutChoice);
-          cin.ignore(10000,'\n');
-
-          if(nutChoice == 'Y')
-          {
-               strncpy(chocolatePref,"Peanut M & M\'s", SNACK_SIZE);
-          }
-          else
-          {
-               strncpy(chocolatePref,"M & M\'s", SNACK_SIZE);
-          }
-     }
-     else
-     {
-          strncpy(chocolatePref,"Skittles", SNACK_SIZE);
-     }
-
-// cerr << "BEGIN TEST"                       << endl
-//      << "nutChoice: "     << nutChoice     << endl
-//      << "chocolatePref: " << chocolatePref << endl
-//      << "END TEST"                         << endl;
-
-     if(age >= 21)
-     {
-          strncpy(drinksPref,"wine", DRINK_SIZE);
-     }
-     else
-     {
-          if(age > 5)
-          {
-               strncpy(drinksPref,"soda", DRINK_SIZE);
-          }
-          else
-          {
-               strncpy(drinksPref,"juice", DRINK_SIZE);
-          }
-     }
-
-// cerr << "BEGIN TEST"                 << endl
-//      << "Age: "        << age        << endl
-//      << "drinksPref: " << drinksPref << endl
-//      << "END TEST"                   << endl;
-
-     cout << endl;
-
-	/***************************************************************************
-     * OUTPUT - Display the user's snacks and drinks 
-     *
-     *         For example:
-     *    What is your friend's name?    Kris Kryzmarcik
-     *    How old is your friend?        5
-     *    Do they like chocholate (Y/N)? Y
-     *    Do they like nuts (Y/N)?       Y
-     * 
-     *    Kris Kryzmarcik should be served Peanut M & M's and juice.
-     ***************************************************************************/
-
-     cout << name << " should be served " << chocolatePref << " and " << drinksPref << '.';
-
-	return 0;
+    cout << "Enter text: ";
+    cin.getline(arraytext, textlength);
+    strcpy(arraychar, arraytext);
 }
 
+// Function to count the argument letter in the text/string
+int countNumbers(char letter, char temp[textlength])
+{
+    int count = 0;
+
+    for (int i = 0; i < textlength; i++)
+    {
+        if (letter == temp[i])
+            count++;
+    }
+    return count;
+
+    /********************* [TO BE ADDED] **********************
+    /Function returns if there are 2 more letters in a string
+    **********************************************************/
+}
+
+// Separating/narrow down repeated letters
+void seperateChar()
+{
+    int i,j, index = 0;
+
+    for (i = 0; i < sizeof(arraychar); i++)
+    {
+        for (j = 0; j < i; j++)
+        {
+            if (arraychar[i] == arraychar[j])
+                break;
+        }
+        if (j == i)
+            arraychar[index++] = arraychar[i];
+    }
+}
+
+// Setting up a data table so I can sort data and stuff to output into Newton's cipher
+void settingUpDataTable()
+{
+    int num;
+
+    seperateChar();
+    strcpy(datatable[0], arraychar);
+
+    for(int i = 0; i < sizeof(arraychar); i++)
+    {
+        num = countNumbers(arraychar[i], arraytext);
+
+        datatable[i][1] = (char)num;
+    }
+}
+
+// output table function
+void table()
+{
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < sizeof(arraychar); j++)
+        {
+            cout << datatable[i][j];
+        }
+        cout << endl;
+    }
+}
+
+int main () 
+{
+    start();
+    settingUpDataTable();
+
+    cout << arraytext << endl << arraychar << endl; // test for text and short text to only non-repeating letters (Banana --> Ban)
+    cout << countNumbers('a', arraytext) << endl;   // test for counts how many 'a' in "Banana" - Will change this soon for the whole string instead of just 'a'
+    table();                                        // outputs the table that DOESN't FUCKING WORK AWIGDIADWIADBIA
+
+
+    return 0;
+}
