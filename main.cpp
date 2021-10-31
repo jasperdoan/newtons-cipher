e44105fd920c7f56319e2c66b4d09d5a7dad0aa8
wluxie <49565505+wluxie@users.noreply.github.com>
2021-10-31 15:38:27 -0700

    -

--- main.cpp
+++ main.cpp
@@ -4,70 +4,38 @@
 #include <algorithm>
 using namespace std;
 
-const int textlength = 7;
+const int textlength = 24;
 string str;            
 char arraytext[textlength];                
 char arraychar[textlength];                 
 
 void calibrate()
 {
+    remove(str.begin(), str.end(), ' ');
     transform(str.begin(), str.end(), str.begin(),[](unsigned char c)
-        { return std::tolower(c); });
+        { return tolower(c); });
 
     sort(str.begin(), str.end());
-
     strcpy(arraytext, str.c_str());
-    strcpy(arraychar, arraytext);
-}
 
-void seperateChar()
-{
-    int i,j, index = 0;
-
-    for (i = 0; i < sizeof(arraychar); i++)
-    {
-        for (j = 0; j < i; j++)
-        {
-            if (arraychar[i] == arraychar[j])
-                break;
-        }
-        if (j == i)
-            arraychar[index++] = arraychar[i];
-    }
+    str.erase(std::unique(str.begin(), str.end()), str.end());
+    strcpy(arraychar, str.c_str());
 }
 
 void quantityCount()
 {
-    int savepoint = 0; 
     int count = 0;
-
     for (int i = 0; i < sizeof(arraychar) - 1; i++)
     {
-        for (int j = savepoint; j < sizeof(arraytext) - 1; j++)
-        {
+        for (int j = 0; j < sizeof(arraytext) - 1; j++)
             if (arraychar[i] == arraytext[j])
-            {
                 count++;
-                if (arraychar[i] != arraytext[j+1])
-                {
-                    cout << count << arraychar[i];
-                    savepoint = savepoint + count;
-                    j = savepoint;
-                    count = 0;
-                    break;
-                }
-                // else
-                // {
-                //     for (int k = 0; k < count; k++)
-                //         cout << arraychar[i];
-
-                //     savepoint = savepoint + count;
-                //     j = savepoint;
-                //     count = 0;
-                //     break;
-                // }
-            }
-        }
+        if (count > 2)
+            cout << count << arraychar[i];
+        else
+            for (int k = 0; k < count; k++) 
+                cout << arraychar[i];
+        count = 0;
     }
 }
 
@@ -75,9 +43,8 @@ int main ()
 {
     cout << "Enter text: ";
     getline(cin, str);
-
+    
     calibrate();
-    seperateChar();
     quantityCount();
     
     return 0;
